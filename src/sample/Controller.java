6cb48ea1cf330f069f0da1601b7c1d65f7fbe51b
Juan C Moreno <jcrogel@gmail.com>
2019-05-23 23:47:35 -0700

    Now with Table

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,22 +3,31 @@ package sample;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class Controller {
     Temperature model;
 
-    @FXML // fx:id="listview";
-            ListView<String> listview;
+    @FXML // fx:id="tableView";
+    TableView<Temperature> tableView;
 
     public void doLoad(){
 
-        ObservableList<String> values =
-                Temperature.getTemperatures(
-                        1880, 2018);
+        ObservableList<Temperature> values =
+                Temperature.getTemperatures(1880, 1881);
 
-        listview.setItems(values);
+        TableColumn<Temperature,String> yearCol
+                = new TableColumn<Temperature, String>("Year");
+        yearCol.setCellValueFactory(new PropertyValueFactory("year"));
+        TableColumn<Temperature,String> valueCol
+                = new TableColumn<Temperature, String>("Value");
+        valueCol.setCellValueFactory(new PropertyValueFactory("value"));
+
+        tableView.getColumns().setAll(yearCol, valueCol);
+        tableView.setItems(values);
     }
 
 
--- src/sample/Temperature.java
+++ src/sample/Temperature.java
@@ -1,31 +1,49 @@
 package sample;
 
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
+import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Temperature {
-    private String year;
-    private Float value;
+    private StringProperty year;
+    private FloatProperty value;
 
     public Temperature(String year, float value){
-        this.year = year;
-        this.value = value;
+        this.year = new SimpleStringProperty(this, "year");
+        this.value = new SimpleFloatProperty(this, "value");
+        this.setYear(year);
+        this.setValue(value);
+    }
+
+    public String getYear() {
+        return year.get();
     }
 
 
-    public static ObservableList<String> getTemperatures(int start, int end){
+    public void setYear(String _year) {
+        this.year.set(_year);
+    }
+
+    public float getValue() {
+        return value.get();
+    }
+
+    public void setValue(float value) {
+        this.value.set(value);
+    }
+
+
+    public static ObservableList<Temperature> getTemperatures(int start, int end){
         String addr = "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/all/1/%d-%d.json";
         addr = String.format(addr, start, end);
 
@@ -40,11 +58,13 @@ public class Temperature {
             JsonObject root = gson.fromJson(reader, JsonObject.class);
             JsonObject data = root.getAsJsonObject("data");
 
-            ObservableList<String> values = FXCollections.observableArrayList();
+            ObservableList<Temperature> values = FXCollections.observableArrayList();
 
             Set<Map.Entry<String, JsonElement>> entrySet = data.entrySet();
             for(Map.Entry<String,JsonElement> entry : entrySet){
-                values.add(entry.getKey()+ " " + entry.getValue().getAsString());
+                String year = entry.getKey();
+                Float value = entry.getValue().getAsFloat();
+                values.add(new Temperature(year, value));
             }
 
             return values;
